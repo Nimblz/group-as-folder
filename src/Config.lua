10ae8d0a2c36bc45c2132a47cd8855cd01a1363c
Nim <areuschle1@gmail.com>
2019-03-04 18:06:22 -0600

    config for plugin

---
+++ src/Config.lua
@@ -0,0 +1,7 @@
+return {
+    TOOLBAR_NAME =  "Nimblz's Plugins",
+    PLUGIN_ID =     "group-as-folder",
+    PLUGIN_NAME =   "Group as Folder",
+    PLUGIN_DESC =   "Groups selection under a Folder, rather than a model",
+    PLUGIN_ICON =   "rbxassetid://0",
+}
